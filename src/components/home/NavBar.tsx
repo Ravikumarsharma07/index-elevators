8c356b21eb3b420dd3813bcf5563fe4bc00a3a27
Ravikumarsharma07 <ravikr.sharma2007@gmail.com>
2025-03-06 19:19:11 +0530

    fixed(eslint) animated hero section

--- src/components/home/NavBar.tsx
+++ src/components/home/NavBar.tsx
@@ -1,6 +1,5 @@
 "use client"
 import { Forklift, MenuIcon, PhoneCall, X } from "lucide-react";
-import Image from "next/image";
 import Link from "next/link";
 import React, { useState } from "react";
 
